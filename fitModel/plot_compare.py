b95c08a0cafeffe0c96b352bda01d901e928c893
Mohsen Hadianpour <mohsenhadianpour@gmail.com>
2019-05-14 15:05:40 +0430

    Add functions to plot estimated and raw firing rate

--- fitModel/plot_compare.py
+++ fitModel/plot_compare.py
@@ -34,100 +34,65 @@ def fr_epoch(path, pos, st, epoch='Enc', al=False):
     return {'rate_stim': rate_stim, 'estimated_rate_stim': estimated_rate_stim}
 
 
-# rate_stim, estimated_rate_stim = fr_epoch('/home/partnerpc9_ib/SpyderDir/Out/Chain1/', 'In', 'Stim', al=True)
-
-
 # A function for comparing firing rate and effective firing rate plot
 # for desired epochs
-
-def fr_plot(path, pos, st, epoch='Enc', al=False):
-    mapper = {'Enc': 'Visual', 'Mem': 'Memory', 'Sac': 'Saccade'}
-
-    if (al == True):
-        data = fr_epoch(path, pos, st, al=True)
-        rate_stim = data['rate_stim']
-        estimated_rate_stim = data['estimated_rate_stim']
-        col = plt.cm.get_cmap('jet', estimated_rate_stim.shape[1])
-        fig = plt.figure()
-        plt.subplot(2, 1, 1)
-        for pl in range(rate_stim.shape[1]):
-            plt.plot(normalize(sig.savgol_filter(np.array(estimated_rate_stim)[:, pl], 11, 7)),
-                     label=rate_stim.columns[pl], c=col(pl))
-        plt.title('Normalized Effective Firing Rate')
-        fig.legend(loc='center right')
-        plt.subplot(2, 1, 2)
-        for pl in range(estimated_rate_stim.shape[1]):
-            plt.plot(normalize(sig.savgol_filter(np.array(rate_stim)[:, pl], 21, 3)),
-                     label=estimated_rate_stim.columns[pl], c=col(pl))
-        plt.title('Normalized Raw Firing Rate')
-        fig.subplots_adjust(right=0.95)
-        fig.suptitle('Visual,Memory,Saccade')
-        plt.show()
+def fr_plot(path, pos, st, write_path, dataSet, epoch='Enc', al=False):
+    data = fr_epoch(path, pos, st, al=al)
+    rate = data[dataSet]
+    if dataSet == 'rate_stim':
+        window = 11
+        order = 3
     else:
-        data = fr_epoch(path, pos, st)
-        rate_stim = data['rate_stim']
-        estimated_rate_stim = data['estimated_rate_stim']
-        col = plt.cm.get_cmap('jet', estimated_rate_stim.shape[1])
-        fig = plt.figure()
-        plt.subplot(2, 1, 1)
-        for pl in range(rate_stim.shape[1]):
-            plt.plot(normalize(sig.savgol_filter(np.array(estimated_rate_stim)[:, pl], 11, 7)),
-                     label=rate_stim.columns[pl], c=col(pl))
-        plt.title('Normalized Effective Firing Rate')
-        fig.legend(loc='center right')
-        plt.subplot(2, 1, 2)
-        for pl in range(estimated_rate_stim.shape[1]):
-            plt.plot(normalize(sig.savgol_filter(np.array(rate_stim)[:, pl], 21, 3)),
-                     label=estimated_rate_stim.columns[pl], c=col(pl))
-        plt.title('Normalized Raw Firing Rate')
-        fig.subplots_adjust(right=0.95)
-        fig.suptitle(mapper[epoch])
-        plt.show()
-
-
-# fr_plot('/home/partnerpc9_ib/SpyderDir/Out/Chain1/', 'In', 'Stim', 'Enc', al=False)
-# fr_plot('/home/partnerpc9_ib/SpyderDir/Out/Chain1/', 'In', 'Stim', 'Mem', al=False)
-# fr_plot('/home/partnerpc9_ib/SpyderDir/Out/Chain1/', 'In', 'Stim', 'Sac', al=False)
-#
-# fr_plot('/home/partnerpc9_ib/SpyderDir/Out/Chain1/', 'In', 'Stim', al=True)
-
+        window = 35
+        order = 3
+    if al:
+        col = plt.cm.get_cmap('jet', rate.shape[1])
+        plt.xlabel('Time (ms)')
+        plt.ylabel('Normalized Firing Rate')
+        for pl in range(rate.shape[1]):
+            plt.plot(normalize(sig.savgol_filter(np.array(rate)[:, pl], window, order)),
+                     label=rate.columns[pl], c=col(pl))
+        plt.savefig(write_path + 'Firing Rates' + '-' + dataSet + '-' + pos + '-' + st + '-' + 'All' + '.svg', dpi=1000)
+        plt.close()
+    else:
+        col = plt.cm.get_cmap('jet', rate.shape[1])
+        for pl in range(rate.shape[1]):
+            plt.plot(normalize(sig.savgol_filter(np.array(rate)[:, pl], window, order)),
+                     label=rate.columns[pl], c=col(pl))
+        plt.savefig(write_path + 'Firing Rates' + '-' + dataSet + '-' + pos + '-' + st + '-' + epoch + '.svg', dpi=1000)
+        plt.close()
 
 ## Correlation matrixes
+def cor_plot(path, pos, st, write_path, dataSet, epoch='Enc', al=False):
+    data = fr_epoch(path, pos, st, epoch, al=al)
+    rate = data[dataSet]
+    correlation_rate = rate.corr()
 
-def cor_plot(data):
-    rate_stim = data['rate_stim']
-    estimated_rate_stim = data['estimated_rate_stim']
-    correlation_rate = rate_stim.corr()
-    correlation_rate_estim = estimated_rate_stim.corr()
-
-    fig, (ax1, ax2) = plt.subplots(1, 2)
-    cax = ax1.matshow(correlation_rate, vmin=-1, vmax=1, cmap=plt.cm.jet)
-    cax2 = ax2.matshow(correlation_rate_estim, vmin=-1, vmax=1, cmap=plt.cm.jet)
-    ticks = np.arange(0, rate_stim.shape[1], 1)
-    ax1.set_xticks(ticks)
-    ax1.set_yticks(ticks)
-    ax1.set_xticklabels(rate_stim.columns)
-    ax1.set_yticklabels(rate_stim.columns)
-    ax1.set_title('Raw Firing Rate')
-    ax2.set_xticks(ticks)
-    ax2.set_yticks(ticks)
-    ax2.set_xticklabels(estimated_rate_stim.columns)
-    ax2.set_yticklabels(estimated_rate_stim.columns)
-    ax2.set_title('Effective Firing Rate')
-    fig.subplots_adjust(right=0.81)
-    cbar_ax = fig.add_axes([0.85, 0.15, 0.05, 0.7])
-    fig.colorbar(cax, cax=cbar_ax)
-    fig.suptitle('Correlation Matrix')
-    plt.show()
-
-
-# cor_plot(fr_epoch('/home/partnerpc9_ib/SpyderDir/Out/Chain1/', 'In', 'Stim', al=True))
-#
-# cor_plot(fr_epoch('/home/partnerpc9_ib/SpyderDir/Out/Chain1/', 'In', 'Stim', 'Mem', al=False))
-# cor_plot(fr_epoch('/home/partnerpc9_ib/SpyderDir/Out/Chain1/', 'In', 'Stim', 'Sac', al=False))
+    plt.matshow(correlation_rate, vmin=-1, vmax=1, cmap=plt.cm.jet)
+    ticks = np.arange(0, rate.shape[1], 1)
+    plt.xticks(ticks, rate.columns, rotation='vertical')
+    plt.yticks(ticks, rate.columns)
+    plt.colorbar()
+    if al:
+        plt.savefig(write_path + 'Correlation Matrix' + '-' + dataSet + '-' + pos + '-' + st + '-' + 'All' + '.svg', dpi=1000)
+        plt.close()
+    else:
+        plt.savefig(write_path + 'Correlation Matrix' + '-' + dataSet + '-' + pos + '-' + st + '-' + epoch + '.svg', dpi=1000)
+        plt.close()
 
 # Scatter plot matrix
-
-# pd.plotting.scatter_matrix(rate_stim, alpha=0.2, figsize=(rate_stim.shape[1], rate_stim.shape[1]), diagonal='kde')
-# pd.plotting.scatter_matrix(estimated_rate_stim, alpha=0.2, figsize=(rate_stim.shape[1], rate_stim.shape[1]),
-#                            diagonal='kde')
+def scatter_plot(path, pos, st, write_path, dataSet, epoch='Enc', al=False):
+    data = fr_epoch(path, pos, st, epoch, al=al)
+    if dataSet == 'rate_stim':
+        typ = 'Raw'
+    else:
+        typ = 'Effective'
+    rate = data[dataSet]
+    pd.plotting.scatter_matrix(rate, alpha=0.2, figsize=(rate.shape[1], rate.shape[1]),
+                               diagonal='kde')
+    if al:
+        plt.savefig(write_path + 'Scatter Plot' + '-' + dataSet + '-' + pos + '-' + st + '-' + typ + '-' + 'All' + '.svg',dpi=1000)
+        plt.close()
+    else:
+        plt.savefig(write_path + 'Scatter Plot' + '-' + dataSet + '-' + pos + '-' + st + '-' + typ + '-' + epoch + '.svg',dpi=1000)
+        plt.close()
