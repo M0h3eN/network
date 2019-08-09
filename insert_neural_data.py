1264c66ce7cc7e7f353c07f3c7d42a809b3ab132
mohsen hadianpour <mohsenhadianpour@gmail.com>
2019-08-09 19:39:59 +0430

    Change threshold value to median

--- insert_neural_data.py
+++ insert_neural_data.py
@@ -152,9 +152,9 @@ file_names = os.listdir(args.write + 'Firing Rate/')
 referencePath = args.write + '/Chain1/MCMCValues/'
 
 # create fit_par partial function
-pearson_par = partial(network_info_writer, *[args, referencePath, 0.6, 'pearson'])
-mutual_par = partial(network_info_writer, *[args, referencePath, 0.6, 'mutualScore'])
-hawkes_par = partial(network_info_writer, *[args, referencePath, 0.6, 'hawkes'])
+pearson_par = partial(network_info_writer, *[args, referencePath, 0.5, 'pearson'])
+mutual_par = partial(network_info_writer, *[args, referencePath, 0.5, 'mutualScore'])
+hawkes_par = partial(network_info_writer, *[args, referencePath, 0.5, 'hawkes'])
 
 # pearson
 start_time = time.time()
