3a03531916537ad48471661be39648ee4103ab12
mohsen hadianpour <mohsenhadianpour@gmail.com>
2019-09-05 18:49:33 +0430

    Add degree distribution plot evaluator

--- commons/plotRelatedFunctions/plot_graph.R
+++ commons/plotRelatedFunctions/plot_graph.R
@@ -3,11 +3,15 @@
 # Created by: mohsen
 # Modified on: 8/7/19
 
+rm(list = ls(all=TRUE))
 source("/home/mohsen/projects/network/commons/tools/basicFunctions.R")
 
 args = commandArgs(TRUE)
 path = as.character(args[1])
+requiredDires = c("Correlation", "Hawkes", "MutualInformation")
 subDirs = list.dirs(path, full.names = FALSE, recursive = FALSE)
+subDirs = intersect(subDirs, requiredDires)
+
 
 for (p in subDirs){
 
@@ -19,22 +23,30 @@ for (p in subDirs){
   }
   setwd(file.path(write_path))
 
-  graph_lists = list.files(path = read_path)[endsWith(list.files(path = read_path), ".gml")]
-  graphDataList = lapply(graph_lists, function(x) read_graph(file = paste(read_path, x, sep = ''), format = "gml"))
+  all_files = list.files(path = read_path)
+  epochs_ordered = unique(sapply(strsplit(all_files[endsWith(all_files, ".gml")], "__"), function(x) x[1]))
+  graphDataList = lapply(epochs_ordered, applyFunToGraphList, path = read_path, all_files = all_files)
 
-  ## Writing Graph individualy
-  for(g in 1:length(graph_lists)){
-    svglite(file = paste('strength-', unlist(strsplit(graph_lists[g], ".", fixed = TRUE))[1] , '.svg', sep=''))
+## Writing Graph individualy - strength
+  for(g in 1:length(epochs_ordered)){
+    svglite(file = paste('strength-', epochs_ordered[g] , '.svg', sep=''))
     graphPlot(graphDataList[[g]], "", "strength")
     dev.off()
   }
 
-  # cluster
+## Writing Graph individualy - hub
+  for(g in 1:length(epochs_ordered)){
+    svglite(file = paste('hub-', epochs_ordered[g] , '.svg', sep=''))
+    graphPlot(graphDataList[[g]], "", "hub")
+    dev.off()
+  }
+
+# cluster
   svglite(file = "out3.svg")
 
-  ## Writing Graph individualy
-  for(g in 1:length(graph_lists)){
-    svglite(file = paste('cluster-', unlist(strsplit(graph_lists[g], ".", fixed = TRUE))[1], '.svg', sep=''))
+## Writing Graph individualy
+  for(g in 1:length(epochs_ordered)){
+    svglite(file = paste('cluster-', epochs_ordered[g], '.svg', sep=''))
     clusterGraph(graphDataList[[g]], "")
     dev.off()
   }
@@ -43,5 +55,24 @@ for (p in subDirs){
 
 }
 
+## Plotting Graph degree distribution
+data_frame = read.csv(file = paste(path, "all_chain_network_info.csv", sep=""), header = TRUE)
+deg_df = data.frame("degree" = data_frame$degrees, "neuron" = data_frame$neuron,
+                    "method" = data_frame$method, "epoch" = data_frame$epoch,
+                    "chain" = data_frame$chain) %>%
+                    group_by(neuron, epoch) %>%
+                    summarise(degree = mean(degree)) %>%
+                    arrange(neuron, epoch)
 
+epochsUnique = unique(deg_df$epoch)
 
+write_path = paste(path, "degreeDistribution", "/", sep = "")
+
+  if(!dir.exists(write_path)){
+      dir.create(file.path(write_path))
+  }
+setwd(file.path(write_path))
+
+for (i in epochsUnique){
+  ggsave(filename = paste(i, ".svg",sep = ""), plot = plot_degree(deg_df, i), width=10, height=8)
+}
